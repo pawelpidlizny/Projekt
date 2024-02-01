4f180e4d7d47db4d152e3e2f196b91f905d13ad8
„Pawel <72276374+pawelpidlizny@users.noreply.github.com>
2024-02-01 20:28:41 +0100

    DebugExample.java

---
+++ Projekt-intro/src/main/java/DebugExample.java
@@ -0,0 +1,22 @@
+public class DebugExample {
+    public static void main(String[] args) {
+        int firstNumber = 20;
+        int secondNumber = 20;
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+
+        if (firstNumber > secondNumber){
+            subtractAndDisplay(firstNumber, secondNumber);
+        } else {
+            sumAndDisplay(firstNumber, secondNumber);
+    }
+}
+private static void sumAndDisplay(int a,int b){
+        int result = a + b;
+        System.out.println(result);
+    }
+    private static void subtractAndDisplay (int a, int b){
+        int result = a + b;
+        System.out.println(result);
+    }
+}

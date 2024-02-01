9276cb657b5f64edc8128d541f0f662643e572b0
„Pawel <72276374+pawelpidlizny@users.noreply.github.com>
2024-02-01 20:30:01 +0100

    MayName

---
+++ Projekt-intro/src/main/java/MayName.java
@@ -0,0 +1,17 @@
+public class MayName {
+    public static void main(String[] args) {
+        String myName = "Paweł";
+        int myFon = 57788999;
+        int myAge = 22;
+        double height = 1.90;
+        char carClass = 'P';
+        boolean isGoodProgrammer = true;
+
+        System.out.println(myName);
+        System.out.println(myFon);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+    }
+}

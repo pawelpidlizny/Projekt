0939cee71d1fe79f1f18d4fa1ec54f95135b447c
„Pawel <72276374+pawelpidlizny@users.noreply.github.com>
2024-02-01 20:27:28 +0100

    HelloWorld.java

---
+++ Projekt-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println( "Hello world");
+        System.out.println( "Mam na imie Paweł");
+        System.out.println( "lat mam 39");
+
+    }
+}
